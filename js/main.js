8ca7706da7ba8242b7ebc9f4296a301ea406111f
Dafiul Haq <dafiulhaq97@gmail.com>
2020-09-04 09:32:29 +0700

    tambahin comment

--- js/main.js
+++ js/main.js
@@ -13,11 +13,14 @@ new Vue({
         }
     },
     computed: {
+        // helper data, jalankan watcher ketika salah satu dari data-nya berubah
         inputChange: function() {
             return [this.url, this.keyword, this.size];
         },
+        // mengambil Youtube video ID dari url video yang diberikan
         videoId: function() {
             if (this.url === "") return false;
+            // tambahkan protokol pada url kalau blm ada
             let url = this.url.search(/^https?:\/\//) !== -1 ? this.url : `http://${this.url}`;
             let params = new URL(url).searchParams;
             if (!params.has("v")) return false;
@@ -25,6 +28,7 @@ new Vue({
         }
     },
     watch: {
+        // jalankan fungsi ketika value inputChange berubah
         inputChange: debounce(async function() {
             this.result = {
                 data: [],
@@ -34,6 +38,7 @@ new Vue({
             let validSize = false;
             if (this.size >= 5 && this.size <= 500) {
                 validSize = true;
+                // kalau size yang di input valid, simpan ke localstorage
                 localStorage.setItem("size", this.size);
             }
             if (this.url === "") return this.info = "Masukkan URL video";
@@ -41,6 +46,7 @@ new Vue({
             if (!validSize) return this.info = "Masukkan angka dengan range 5 - 500";
             if (this.keyword) {
                 if (this.keyword.length >= 2) {
+                    // load ulang result kalau semua sudah valid
                     await this.load();
                 } else {
                     this.info = "Kata kunci minimal 2 karakter";
@@ -49,6 +55,8 @@ new Vue({
                 this.info = "Masukkan kata kunci yang ingin dicari";
             }
         }, 500),
+        // selalu simpan ke localstorage setiap url dan expand berubah
+        // walaupun blm dicek valid atau tidak
         url: debounce(function(url) {
             localStorage.setItem("url", url);
         }, 300),
@@ -57,6 +65,7 @@ new Vue({
         }
     },
     created: function() {
+        // ambil data yang disimpan localstorage setiap instance Vue dibuat
         const url = localStorage.getItem("url");
         const size = localStorage.getItem("size");
         const expand = localStorage.getItem("expand");
@@ -70,6 +79,7 @@ new Vue({
     },
     methods: {
         async load() {
+            // informasi loading sementara melakukan fetch
             this.info = "...";
 
             const queries = {
@@ -81,18 +91,22 @@ new Vue({
                 page: this.result.page + 1
             };
             const requestUrl = "http://yt-transcripts.vercel.app/api?" + this.serialize(queries);
+            // ambil data json melalui fetch ke 'requestUrl'
             const respond = await fetch(requestUrl).then((res) => (res.ok ? res.json() : []));
 
             if (respond.data.length > 0) {
+                // gabungkan data sebelumnya dengan data yang baru di fetch
                 const combinedData = this.result.data.concat(respond.data);
                 this.info = `Menampilkan ${combinedData.length} dari ${respond.search.found} hasil ditemukan`;
                 this.result.data = combinedData;
                 this.result.page = respond.page;
+                // kalau page saat ini sama dengan total pages, berarti sudah page terakhir
                 this.result.isLast = (respond.page === respond.total_pages);
             } else {
                 this.info = `Tidak menemukan hasil dengan kata kunci "${this.keyword}"`;
             }
         },
+        // mengubah angka (detik) yang di input ke format mm:ss / hh:mm:ss
         formatTime(seconds) {
             const milliseconds = seconds * 1000;
             let result = new Date(milliseconds).toISOString().substr(11, 8);
@@ -101,6 +115,7 @@ new Vue({
             }
             return result.startsWith("0") ? result.substr(1) : result;
         },
+        // ubah object yang di input ke query string
         serialize(obj) {
             let qs = [];
             for (let key in obj) {
@@ -116,6 +131,7 @@ new Vue({
     }
 });
 
+// saat development tdk perlu menggunakan service worker
 if ('serviceWorker' in navigator && !["localhost", "127.0.0.1"].includes(location.hostname)) {
     navigator.serviceWorker.register('/sw.js').then((data) => {
         console.log("Registrasi service worker berhasil!");
@@ -124,6 +140,8 @@ if ('serviceWorker' in navigator && !["localhost", "127.0.0.1"].includes(locatio
     });
 }
 
+// membatasi pemanggilan suatu fungsi untuk waktu yang ditentukan
+// https://github.com/sindresorhus/p-debounce
 function debounce(fn, wait) {
     let timer;
     let resolveList = [];
